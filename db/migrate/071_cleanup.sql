a2fbb061831393dfe08e656399c1d49988dc3c9e
Vu Nguyen <olvrng@gmail.com>
2019-07-30 11:44:36 +0700

    cleanup: actually drop unused tables
    Change-Id: Icbe0f619561a24d5c21abaa77d194db8334a3c0d

--- db/migrate/071_cleanup.sql
+++ db/migrate/071_cleanup.sql
@@ -11,3 +11,39 @@ ALTER TABLE supplier RENAME TO _supplier;
 ALTER TABLE variant_external RENAME TO _variant_external;
 
 ALTER TABLE product_source_category RENAME to shop_category;
+
+ALTER TABLE order_line DROP CONSTRAINT order_line_variant_id_fkey;
+ALTER TABLE order_line DROP CONSTRAINT order_line_product_id_fkey;
+ALTER TABLE order_line DROP CONSTRAINT order_line_supplier_id_fkey;
+ALTER TABLE shop_variant DROP CONSTRAINT shop_variant_product_id_fkey;
+ALTER TABLE product_shop_collection DROP CONSTRAINT product_shop_collection_product_id_fkey;
+ALTER TABLE shop DROP CONSTRAINT shop_product_source_id_fkey;
+ALTER TABLE shop DROP CONSTRAINT shop_order_source_id_fkey;
+ALTER TABLE shop_category DROP CONSTRAINT product_source_category_product_source_id_fkey;
+ALTER TABLE money_transaction_shipping DROP CONSTRAINT money_transaction_supplier_id_fkey;
+ALTER TABLE fulfillment DROP CONSTRAINT fulfillment_supplier_id_fkey;
+ALTER TABLE credit DROP CONSTRAINT credit_supplier_id_fkey;
+
+ALTER TABLE _supplier DROP CONSTRAINT supplier_product_source_id_fkey;
+ALTER TABLE _product DROP CONSTRAINT product_product_brand_id_fkey;
+ALTER TABLE _product DROP CONSTRAINT product_supplier_id_fkey;
+ALTER TABLE _product DROP CONSTRAINT product_etop_category_id_fkey;
+ALTER TABLE _variant DROP CONSTRAINT variant_product_brand_id_fkey;
+ALTER TABLE _variant DROP CONSTRAINT variant_supplier_id_fkey;
+ALTER TABLE _variant DROP CONSTRAINT variant_etop_category_id_fkey;
+
+DROP TABLE product_source_internal;
+DROP TABLE _product_external;
+DROP TABLE _variant_external;
+DROP TABLE _product_brand;
+DROP TABLE _product_source_category_external;
+DROP TABLE _product_source;
+DROP TABLE _supplier;
+DROP TABLE _order_external;
+DROP TABLE _order_source;
+DROP TABLE _etop_category;
+
+-- be careful, we want to keep them for safety!
+--
+-- DROP TABLE _variant;
+-- DROP TABLE _product;
