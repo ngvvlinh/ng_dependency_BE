fcefb1657f464ecdcfa81a2e5067d18fd278a2e8
Vu Nguyen <olvrng@gmail.com>
2019-09-23 09:23:36 +0700

    ci: run more 'go mod tidy' in verify-generate.sh
    Change-Id: I67f19d5a99bb072011a082b072559835928ddfa6

--- scripts/ci/verify-generate.sh
+++ scripts/ci/verify-generate.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 set -eo pipefail
+wd=$(pwd)
+
+tidy() {
+  cd $1 && go mod tidy && cd $wd
+}
 
 scripts/protobuf-gen.sh
 scripts/generate-all.sh
+
 go mod tidy
+tidy $wd/up/etop.vn/api
+tidy $wd/up/etop.vn/capi
+tidy $wd/up/etop.vn/common
+
 CHANGES="$(git status -s)"
 if [[ ! -z "$CHANGES" ]]; then
   echo "$CHANGES"
