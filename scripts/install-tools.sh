508805b646513a3bdfbe150d5ed966126f8de207
Vu Nguyen <olvrng@gmail.com>
2019-10-10 14:29:04 +0700

    ci(scripts): pin install-tools version
    Change-Id: Iaaae19ac066e116b18dd3ffd28a71109b875d4cc

--- scripts/install-tools.sh
+++ scripts/install-tools.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
-go get github.com/elliots/protoc-gen-twirp_swagger
-go get github.com/gogo/protobuf/protoc-gen-gogo
-go get github.com/jteeuwen/go-bindata/go-bindata
-go get github.com/twitchtv/twirp/protoc-gen-twirp
-go get golang.org/x/tools/cmd/goimports
+#!/bin/bash
+go get github.com/elliots/protoc-gen-twirp_swagger@fda01df7c6eb
+go get github.com/gogo/protobuf/protoc-gen-gogo@v1.3.0
+go get github.com/jteeuwen/go-bindata/go-bindata@v3.0.7
+go get github.com/twitchtv/twirp/protoc-gen-twirp@v5.8.0
+go get golang.org/x/tools/cmd/goimports@release-branch.go1.13
