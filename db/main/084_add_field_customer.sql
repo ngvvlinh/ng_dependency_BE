9eee05c4aaef09cc8a01719edc09b5ebc89e49c3
nguyentuan <nguyentuanbk92@gmail.com>
2019-10-15 15:31:35 +0700

    fix(migration): rename and fix sql migration
    Change-Id: Ic6afd380bf3eeb4bc24b7f00b5b42288cb077a7e

--- db/main/084_add_field_customer.sql
+++ db/main/084_add_field_customer.sql
@@ -1,4 +1,5 @@
 alter table shop_customer add column full_name_norm tsvector;
+alter table history.shop_customer add column full_name_norm tsvector;
 
 CREATE INDEX ON shop_customer USING gin(full_name_norm);
 CREATE INDEX ON shop_customer (phone);
