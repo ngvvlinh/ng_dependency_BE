370fda00cf852013bb8ad52a7aeffb7a6a9b8a3b
Nam <huynhhainam96qt@gmail.com>
2019-12-24 15:50:34 +0700

    fix(summary): all order belong to etop_pos and order_source_type update created_by to shop.owner_id
    Change-Id: I034341a626913ffd1d24b6c22eea69340654dba4

---
+++ scripts/once/update_created_by_order/main.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"flag"
+
+	"etop.vn/backend/cmd/etop-server/config"
+	ordermodel "etop.vn/backend/com/main/ordering/model"
+	cm "etop.vn/backend/pkg/common"
+	cc "etop.vn/backend/pkg/common/config"
+	"etop.vn/backend/pkg/common/sql/cmsql"
+	"etop.vn/capi/dot"
+	"etop.vn/common/l"
+)
+
+var (
+	ll  = l.New()
+	cfg config.Config
+	db  *cmsql.Database
+)
+
+type M map[string]interface{}
+
+func main() {
+	cc.InitFlags()
+	flag.Parse()
+	var err error
+	if cfg, err = config.Load(false); err != nil {
+		ll.Fatal("Error while loading config", l.Error(err))
+	}
+	cm.SetEnvironment(cfg.Env)
+
+	postgres := cfg.Postgres
+	if db, err = cmsql.Connect(postgres); err != nil {
+		ll.Fatal("Error while connecting database", l.Error(err))
+	}
+	var errs []error
+	count, updated := 0, 0
+
+	var orderID dot.ID
+	orderID = 1
+	for {
+		orders, err := scanOrderID(orderID)
+		if err != nil {
+			ll.Fatal("Error", l.Error(err))
+		}
+		if len(orders) == 0 {
+			ll.S.Infof("Done: updated order %v/%v", updated, count)
+			break
+		}
+		count += len(orders)
+		err = UpdateOrderCreatedBy(orderID, orders[len(orders)-1].ID)
+		if err != nil {
+			errs = append(errs, err)
+			continue
+		}
+		orderID = orders[len(orders)-1].ID
+		updated += len(orders)
+		ll.S.Infof("Updated order %v/%v", updated, count)
+	}
+	for _, err = range errs {
+		ll.S.Errorf("Error: %v", err)
+	}
+}
+
+func scanOrderID(orderID dot.ID) (orders ordermodel.Orders, err error) {
+	err = db.
+		Where("id > ?", orderID.Int64()).
+		Where("order_source_type = 'etop_pos' or order_source_type = 'ts_app'").
+		Where("created_by is null").
+		OrderBy("id").
+		Limit(1000).
+		Find(&orders)
+	return orders, err
+}
+
+func UpdateOrderCreatedBy(idFrom dot.ID, idTo dot.ID) (err error) {
+	if _, err = db.SQL(`
+		update "order" set
+		created_by = shop.owner_id
+		from shop
+		where
+		(
+		"order".order_source_type = 'etop_pos' 
+			or "order".order_source_type = 'ts_app') and 
+		"order".shop_id = shop.id and
+		created_by is null and
+		"order".id >= ?
+		and "order".id <= ?
+    `, idFrom, idTo).Exec(); err != nil {
+		return err
+	}
+	return nil
+}
