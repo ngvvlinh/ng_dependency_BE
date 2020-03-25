bdd5ffc608a2d80e6c0825896188a5553f3b0749
builamquangngoc91 <builamquangngoc91@gmail.com>
2020-03-25 12:04:42 +0700

    fix(webhook): panic ID unexpected
    Change-Id: I2737aeca5503466962717658044080dd007975c5

--- up/etop.vn/capi/dot/util.go
+++ up/etop.vn/capi/dot/util.go
@@ -7,7 +7,7 @@ import (
 func JoinIDs(ids []ID) string {
 	b := make([]byte, 0, 20*len(ids))
 	for _, id := range ids {
-		strconv.AppendInt(b, id.Int64(), 10)
+		b = strconv.AppendInt(b, id.Int64(), 10)
 		b = append(b, ',')
 	}
 	if len(b) > 0 {
