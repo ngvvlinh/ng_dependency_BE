492bccbc30dd24332eb8cb56360e38079ef2357c
Nam <huynhhainam96qt@gmail.com>
2020-07-08 14:48:14 +0700

    fix(sms): send sms in dev faboshop
    Change-Id: I26db628eda723fe1bff758c224d5bc892980f38d
    Reviewed-on: https://g.meta.etop.vn/c/etop/backend/+/3004
    Tested-by: eTop Bot
    Reviewed-by: Tuấn Phan (BE) <nguyentuanbk92@gmail.com>

--- cogs/sms/_min/wire.go
+++ cogs/sms/_min/wire.go
@@ -6,6 +6,7 @@ import (
 	_sms "o.o/backend/cogs/sms"
 	"o.o/backend/pkg/integration/sms"
 	"o.o/backend/pkg/integration/sms/mock"
+	"o.o/backend/pkg/integration/sms/telegram"
 	"o.o/backend/pkg/integration/sms/vietguys"
 )
 
@@ -18,6 +19,8 @@ func SupportedSMSDrivers(cfg sms.Config) []sms.DriverConfig {
 	var mainDriver sms.Driver
 	if cfg.Mock {
 		mainDriver = mock.GetMock()
+	} else if cfg.Telegram {
+		mainDriver = telegram.GetTelegram()
 	} else {
 		mainDriver = vietguys.New(cfg.Vietguys)
 	}
