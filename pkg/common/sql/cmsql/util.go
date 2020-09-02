9f4d31b4f56134a273eba178d789c50b913fec1c
Vu Nguyen <olvrng@gmail.com>
2020-09-02 15:40:33 +0700

    chore(db): add postgres mock
    Change-Id: I257cd9f4ab76df1d14562a802af39374f4ef1e40
    Reviewed-on: https://g.meta.etop.vn/c/etop/backend/+/3811
    Tested-by: eTop Bot
    Reviewed-by: Vu Nguyen <olvrng@gmail.com>

--- pkg/common/sql/cmsql/util.go
+++ pkg/common/sql/cmsql/util.go
@@ -3,9 +3,21 @@ package cmsql
 import (
 	"context"
 
+	cm "o.o/backend/pkg/common"
 	"o.o/backend/pkg/common/sql/sq"
 	"o.o/backend/pkg/common/sql/sqltrace"
 )
+
+func MockPostgresDB() *Database {
+	db := sq.Database{}
+	sq.DollarMarker(&db)
+	sq.DoubleQuoteEscape(&db)
+	sq.UseArrayInsteadOfJSON(&db, true)
+	return &Database{
+		id: cm.NewID().Int64(),
+		db: db,
+	}
+}
 
 type QueryFactory func() QueryInterface
 
