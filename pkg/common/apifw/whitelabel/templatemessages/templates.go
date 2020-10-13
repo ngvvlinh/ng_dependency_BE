d2946f06b4847943c834cfbe638900859cf0d16f
tuan <tuan@eye-solution.vn>
2020-10-13 09:28:17 +0700

    fix(sms): fix template sms send invitation
    Change-Id: If3ad1fcddf7082fdc10f3d4e03535d0179b7c234

--- pkg/common/apifw/whitelabel/templatemessages/templates.go
+++ pkg/common/apifw/whitelabel/templatemessages/templates.go
@@ -81,9 +81,9 @@ Nếu bạn không nhận ra cửa hàng trên, hãy bỏ qua email này.<br><br
 Đội ngũ {{.WlName}}
 `))
 
-var PhoneInvitationTpl = template.Must(template.New("phone-verification").Parse(`Bạn được mời vào cửa hàng {{.ShopName}} trên etop. Bấm vào liên kết sau để xác nhận {{.URL}}`))
+var PhoneInvitationTpl = template.Must(template.New("phone-verification").Parse(`Bạn được mời vào cửa hàng {{.ShopName}} trên etop. Hãy bấm vào liên kết sau để xác nhận {{.URL}}`))
 
-var PhoneInvitationTplRepeat = template.Must(template.New("phone-verification").Parse(`Bạn được mời vào cửa hàng {{.ShopName}} trên etop. Bấm vào liên kết sau để xác nhận {{.URL}} (gửi lần {{.SendTime}})`))
+var PhoneInvitationTplRepeat = template.Must(template.New("phone-verification").Parse(`Bạn được mời vào cửa hàng {{.ShopName}} trên etop. Hãy bấm vào liên kết sau để xác nhận {{.URL}} (gửi lần {{.SendTime}})`))
 
 var EmailSTokenTpl = template.Must(template.New("email-verification").Parse(`
 Gửi {{.FullName}},<br><br>
