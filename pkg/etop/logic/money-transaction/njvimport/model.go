fc10948915d573212aae63ee550fdafad6c3b092
builamquangngoc91 <builamquangngoc91@gmail.com>
2020-10-30 16:38:13 +0700

    fix(monentx): fix import money transaction NJV
    Change-Id: Ife54454ef8cc23231b03cd79ae212419302eec29

--- pkg/etop/logic/money-transaction/njvimport/model.go
+++ pkg/etop/logic/money-transaction/njvimport/model.go
@@ -7,14 +7,25 @@ import (
 
 var schema = imcsv.Schema{
 	{
-		Name:    "tracking_id",
-		Display: "tracking_id",
-		Norm:    "tracking id",
+		Name:     "no",
+		Display:  "No.",
+		Norm:     "no",
+		Optional: true,
 	},
 	{
-		Name:    "created",
-		Display: "Created",
-		Norm:    "created",
+		Name:    "order_no",
+		Display: "Order No.",
+		Norm:    "order no",
+	},
+	{
+		Name:    "cod_amount",
+		Display: "COD Amount",
+		Norm:    "cod amount",
+	},
+	{
+		Name:    "date_of_deliver",
+		Display: "Date of deliver",
+		Norm:    "date of deliver",
 	},
 	{
 		Name:    "status",
@@ -22,115 +33,21 @@ var schema = imcsv.Schema{
 		Norm:    "status",
 	},
 	{
-		Name:    "updated",
-		Display: "Updated",
-		Norm:    "updated",
-	},
-	{
-		Name:    "from_address_1",
-		Display: "From address 1",
-		Norm:    "from address 1",
-	},
-	{
-		Name:    "to_address_1",
-		Display: "To_address1",
-		Norm:    "to address1",
-	},
-	{
-		Name:    "from_address_2",
-		Display: "From address 2",
-		Norm:    "from address 2",
-	},
-	{
-		Name:    "to_address_2",
-		Display: "To_address2",
-		Norm:    "to address2",
-	},
-	{
-		Name:    "to_address_3",
-		Display: "To_address3",
-		Norm:    "to address3",
-	},
-	{
-		Name:    "delivery_type_id",
-		Display: "delivery_type_id",
-		Norm:    "delivery type id",
-	},
-	{
-		Name:    "weight",
-		Display: "Weight (kg)",
-		Norm:    "weight kg",
-	},
-	{
-		Name:    "cod",
-		Display: "COD",
-		Norm:    "cod",
-	},
-	{
-		Name:    "classification",
-		Display: "Classification",
-		Norm:    "classification",
-	},
-	{
-		Name:    "zones",
-		Display: "Zones",
-		Norm:    "zones",
-	},
-	{
-		Name:    "main_fee",
-		Display: "Fees",
-		Norm:    "fees",
-	},
-	{
-		Name:    "rts_fee",
-		Display: "RTS",
-		Norm:    "rts",
-	},
-	{
-		Name:    "ins_fee",
-		Display: "ins",
-		Norm:    "ins",
-	},
-	{
-		Name:    "vat_fee",
-		Display: "VAT",
-		Norm:    "vat",
-	},
-	{
-		Name:    "cod_fee",
-		Display: "Phí thu hộ",
-		Norm:    "phi thu ho",
-	},
-	{
-		Name:    "total_fee",
-		Display: "Total",
-		Norm:    "total",
+		Name:    "report_sent_date",
+		Display: "Report sent date",
+		Norm:    "report sent date",
 	},
 }
 
 type indexes struct {
 	indexer imcsv.Indexer
 
-	trackingID     int
-	created        int
-	status         int
-	updated        int
-	fromAddress1   int
-	toAddress1     int
-	fromAddress2   int
-	toAddress2     int
-	toAddress3     int
-	deliveryTypeID int
-	weight         int
+	no             int
+	orderNo        int
 	codAmount      int
-	classification int
-	zones          int
-	mainFee        int
-	rtsFee         int
-	insFee         int
-	vatFee         int
-	codFee         int
-	totalFee       int
+	dateOfDeliver  int
+	status         int
+	reportSentDate int
 }
 
 var (
@@ -144,36 +61,17 @@ var (
 func initIndexes(schema imcsv.Schema) indexes {
 	indexer := schema.Indexer()
 	return indexes{
-		trackingID:     indexer("tracking_id"),
-		created:        indexer("created"),
+		no:             indexer("no"),
+		orderNo:        indexer("order_no"),
+		codAmount:      indexer("cod_amount"),
+		dateOfDeliver:  indexer("date_of_deliver"),
 		status:         indexer("status"),
-		updated:        indexer("updated"),
-		fromAddress1:   indexer("from_address_1"),
-		toAddress1:     indexer("to_address_1"),
-		fromAddress2:   indexer("from_address_2"),
-		toAddress2:     indexer("to_address_2"),
-		toAddress3:     indexer("to_address_3"),
-		deliveryTypeID: indexer("delivery_type_id"),
-		weight:         indexer("weight"),
-		codAmount:      indexer("cod"),
-		classification: indexer("classification"),
-		zones:          indexer("zones"),
-		mainFee:        indexer("main_fee"),
-		rtsFee:         indexer("rts_fee"),
-		insFee:         indexer("ins_fee"),
-		vatFee:         indexer("vat_fee"),
-		codFee:         indexer("cod_fee"),
-		totalFee:       indexer("total_fee"),
+		reportSentDate: indexer("report_sent_date"),
 	}
 }
 
 type RowMoneyTransaction struct {
 	ExCode    string
-	MainFee   float64
-	RtsFee    float64 // return fee
-	InsFee    float64 // insurance fee
-	VatFee    float64 // VAT fee
-	CodFee    float64
 	CodAmount float64
 }
 
