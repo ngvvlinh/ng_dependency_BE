4e341cc9edc066b19b5d5c634ff80eec4d800564
tuan <tuan@eye-solution.vn>
2020-11-07 09:48:29 +0700

    fix(sms): fix fabo sms template
    Change-Id: Id065fec1ac082dbb8b8ed3e1e66caf274f1b62dc

--- pkg/common/apifw/whitelabel/fabo/templatemessages.go
+++ pkg/common/apifw/whitelabel/fabo/templatemessages.go
@@ -17,7 +17,7 @@ func InitTemplateMsg() {
 
 	templatemessages.SmsResetPasswordTplRepeat = `Nhập mã %v để khôi phục mật khẩu tài khoản Faboshop của bạn. Mã có hiệu lực trong 1 giờ. Vui lòng không chia sẻ cho bất kỳ ai. (gửi lần %v)`
 
-	templatemessages.PhoneInvitationTpl = template.Must(template.New("phone-verification").Parse(`Bạn được mời vào cửa hàng {{.ShopName}} trên Faboshop. Hãy bấm vào liên kết sau để xác nhận {{.URL}}`))
+	templatemessages.PhoneInvitationTpl = template.Must(template.New("phone-verification").Parse(`Bạn được mời vào cửa hàng {{.ShopName}} trên Faboshop. Bấm vào liên kết sau để xác nhận: {{.URL}}`))
 
-	templatemessages.PhoneInvitationTplRepeat = template.Must(template.New("phone-verification").Parse(`Bạn được mời vào cửa hàng {{.ShopName}} trên Faboshop. Hãy bấm vào liên kết sau để xác nhận {{.URL}} (gửi lần {{.SendTime}})`))
+	templatemessages.PhoneInvitationTplRepeat = template.Must(template.New("phone-verification").Parse(`Bạn được mời vào cửa hàng {{.ShopName}} trên Faboshop. Bấm vào liên kết sau để xác nhận: {{.URL}} (gửi lần {{.SendTime}})`))
 }
