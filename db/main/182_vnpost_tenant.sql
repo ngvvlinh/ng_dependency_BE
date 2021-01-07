b089b2552843acdcd78d088d16ab52451470d276
tuan <tuan@eye-solution.vn>
2021-01-07 17:49:41 +0700

    fix(etelecom): fix file migration tenant
    Change-Id: I42387b5a0474cd2b082a23785cb7a7a7a93cc5fb

--- db/main/182_vnpost_tenant.sql
+++ db/main/182_vnpost_tenant.sql
@@ -1,4 +1,4 @@
-INSERT INTO "public"."connection" ("id", "name", "status", "partner_id", "created_at", "updated_at", "deleted_at", "driver_config", "driver", "connection_type", "connection_subtype", "connection_method", "connection_provider", "etop_affiliate_account", "code") VALUES ('1000632092361806111', 'Etelecom', '1', NULL, NOW(), NOW(), NULL, NULL, 'crm/_/direct/vht', 'telecom', NULL, 'direct', 'vht', NULL, 'F57J');
+INSERT INTO "public"."connection" ("id", "name", "status", "partner_id", "created_at", "updated_at", "deleted_at", "driver_config", "driver", "connection_type", "connection_subtype", "connection_method", "connection_provider", "etop_affiliate_account", "code") VALUES ('1000632092361806111', 'Etelecom', '1', NULL, NOW(), NOW(), NULL, NULL, 'crm/_/direct/portsip', 'telecom', NULL, 'direct', 'portsip', NULL, 'F57J');
 
 -- shop connection for vnpost shop
 INSERT INTO shop_connection(owner_id, connection_id, token, status, is_global, created_at, updated_at, telecom_data, token_expires_at, last_sync_at)
