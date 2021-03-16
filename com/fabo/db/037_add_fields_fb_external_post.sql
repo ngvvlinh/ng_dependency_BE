be5ea4786d5e573d3d392975f667940d9c512717
Ngoc <builamquangngoc91@gmail.com>
2021-03-16 11:11:04 +0700

    fix(fabo): add missing sql
    Change-Id: Idc551dde61911fd10b42e59299bd6f087f481eff

--- com/fabo/db/037_add_fields_fb_external_post.sql
+++ com/fabo/db/037_add_fields_fb_external_post.sql
@@ -4,3 +4,12 @@ ALTER TABLE "order"
 ALTER TABLE history."order"
     ADD COLUMN external_comment_id TEXT,
     ADD COLUMN external_post_id TEXT;
+
+ALTER TABLE fb_external_post
+    ADD COLUMN status_type INT,
+    ADD COLUMN total_comments INT,
+    ADD COLUMN total_reactions INT;
+ALTER TABLE history.fb_external_post
+    ADD COLUMN status_type INT,
+    ADD COLUMN total_comments INT,
+    ADD COLUMN total_reactions INT;
