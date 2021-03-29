44271b880e4635fb2cec44b5626ba2831dbe76a5
Ngoc <builamquangngoc91@gmail.com>
2021-03-29 15:35:50 +0700

    feat(payme): Change Method of callback-url payme
    Change-Id: I7125b1c219d4112ed422dc8068ea68de50238c61
    Reviewed-on: https://g.meta.etop.vn/c/etop/backend/+/4927
    Tested-by: eTop Bot
    Reviewed-by: Tuấn Phan (BE) <nguyentuanbk92@gmail.com>

--- cogs/shipment/webhook/_all/cogs.go
+++ cogs/shipment/webhook/_all/cogs.go
@@ -1,13 +1,20 @@
 package _all
 
 import (
+	"io/ioutil"
 	"net/http"
-	
+
+	cm "o.o/backend/pkg/common"
 	"o.o/backend/pkg/common/apifw/httpx"
 	"o.o/backend/pkg/integration/shipping/ghn"
 	ghnwebhookv1 "o.o/backend/pkg/integration/shipping/ghn/webhook/v1"
 	ghnwebhookv2 "o.o/backend/pkg/integration/shipping/ghn/webhook/v2"
 	njvwebhook "o.o/backend/pkg/integration/shipping/ninjavan/webhook"
+	"o.o/common/l"
+)
+
+var (
+	ll = l.New()
 )
 
 type ShipmentWebhookServer *http.Server
@@ -27,11 +34,21 @@ func NewShipmentWebhookServer(
 	ghnwebhookv2.Register(rt)
 	// ninjavan
 	njvwh.Register(rt)
-	
+
 	// test callback url for payment
-	rt.GET("/callback-url/payment/payme", func(c *httpx.Context) error {
-		r := c.Req
-		c.SetResult(r.URL.Query())
+	rt.POST("/callback-url/payment/payme", func(c *httpx.Context) error {
+		body, err := ioutil.ReadAll(c.Req.Body)
+		if err != nil {
+			return cm.Error(cm.InvalidArgument, err.Error(), err)
+		}
+
+		ll.SendMessage("payme: ", string(body))
+
+		defer func() {
+			writer := c.SetResultRaw()
+			writer.Header().Set("Content-Type", "application/json")
+			writer.WriteHeader(200)
+		}()
 		return nil
 	})
 
